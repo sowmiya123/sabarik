87d1194029087dde0e7a0cdd2dd8f8c71241651c
sowmiya123 <30368541+sowmiya123@users.noreply.github.com>
2017-10-13 14:30:44 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char z;
+printf("enter a character");
+scanf("%c",&z);
+if((z=a||z=e||z=i||z=o||z=u)||(z=A||z=E||z=I||z=O||z=U))
+{
+printf("vowel");
+}
+else
+{
+printf("consonant");
+}
+return 0;
+}
